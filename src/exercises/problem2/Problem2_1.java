02d249249b5d686b17494fe011aee8a73a10c14e
s-baji <shuntaro-baji@froide.co.jp>
2021-04-28 19:59:54 +0900

    problem2_1

--- src/exercises/problem2/Problem2_1.java
+++ src/exercises/problem2/Problem2_1.java
@@ -7,5 +7,9 @@ public class Problem2_1 {
         System.out.println("a = " + a);
         System.out.println("b = " + b);
         System.out.println(a + " + " + b + " = " + (a + b));
+        System.out.println(a + " - " + b + " = " + (a - b));
+        System.out.println(a + " * " + b + " = " + (a * b));
+        System.out.println(a + " / " + b + " = " + (a / b));
+        System.out.println(a + " % " + b + " = " + (a % b));
     }
 }
