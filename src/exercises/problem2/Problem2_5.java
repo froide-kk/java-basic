106117084c9cb0c6c103e41933f7103098f4c274
s-baji <shuntaro-baji@froide.co.jp>
2021-05-06 00:16:47 +0900

    problem sample

--- src/exercises/problem2/Problem2_5.java
+++ src/exercises/problem2/Problem2_5.java
@@ -5,9 +5,24 @@ public class Problem2_5 {
         String temp = "こんばんわおはよう";
         String ex_temp = "こおはんばんようわ";
         // tempを「おはよう」と「こんばんわ」に分割
-
+        String morning = temp.replace("こんばんわ","");
+        String evening = temp.replace("おはよう","");
         // ex_tempを「おはよう」と「こんばんわ」に分割
+        String ex_morning = ex_temp.replace("こ","");
+        ex_morning = ex_morning.replace("ん","");
+        ex_morning = ex_morning.replace("ば","");
+        ex_morning = ex_morning.replace("わ","");
 
-        System.out.println();
+        String ex_evening = ex_temp.replace("お","");
+        ex_evening = ex_evening.replace("は","");
+        ex_evening = ex_evening.replace("よ","");
+        ex_evening = ex_evening.replace("う","");
+
+        System.out.println("ans1 " + morning);
+        System.out.println("ans2 " + evening);
+
+        System.out.println("ex_ans1 " + ex_morning);
+        System.out.println("ex_ans2 " + ex_evening);
+
     }
 }
