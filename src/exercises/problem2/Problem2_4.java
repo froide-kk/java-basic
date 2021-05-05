21e897c6160ef91ccc52989acc9f2b32c61f6e55
s-baji <shuntaro-baji@froide.co.jp>
2021-05-05 20:46:15 +0900

    練習問題追加

---
+++ src/exercises/problem2/Problem2_4.java
@@ -0,0 +1,9 @@
+package exercises.problem2;
+
+public class Problem2_4 {
+    public static void main(String[] args){
+        String temp = "こんにちわ、今日は良い天気ですね";
+        // 最後の5文字を取得
+        System.out.println();
+    }
+}
---
+++ src/exercises/problem2/Problem2_5.java
@@ -0,0 +1,13 @@
+package exercises.problem2;
+
+public class Problem2_5 {
+    public static void main(String[] args){
+        String temp = "こんばんわおはよう";
+        String ex_temp = "こおはんばんようわ";
+        // tempを「おはよう」と「こんばんわ」に分割
+
+        // ex_tempを「おはよう」と「こんばんわ」に分割
+
+        System.out.println();
+    }
+}
