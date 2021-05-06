5db9d32541bb3fee9245c368ec6e7701bc2136b4
池田りお <rio-ikeda@froide.co.jp>
2021-05-06 11:21:10 +0900

    Chapter1変更

--- src/chapters/ch01/Calculator.java
+++ src/chapters/ch01/Calculator.java
@@ -3,10 +3,10 @@ package chapters.ch01;
 public class Calculator {
 	public static void main(String[] args) {
 		/* 計算をする */
-		int a = 1;
-		int b = 2;
+		int a = 2;
+		int b = 8;
 		int c = a + b; // cは3になる
 		/* 計算した結果を表示する */
-		System.out.println("1 + 2 = " + c);
+		System.out.println("2 + 8 = " + c);
 	}
 }
