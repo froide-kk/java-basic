8846c55baa350f5ccdb1edea655e54e4d9b3462e
froide-minori <minori-hagihara@froide.co.jp>
2021-05-06 11:23:57 +0900

    chapter1 変更

--- src/chapters/ch01/Calculator.java
+++ src/chapters/ch01/Calculator.java
@@ -3,10 +3,10 @@ package chapters.ch01;
 public class Calculator {
 	public static void main(String[] args) {
 		/* 計算をする */
-		int a = 1;
-		int b = 2;
-		int c = a + b; // cは3になる
+		int a = 4;
+		int b = 5;
+		int c = a * b; // cは3になる
 		/* 計算した結果を表示する */
-		System.out.println("1 + 2 = " + c);
+		System.out.println("4 × 5 = " + c);
 	}
 }
