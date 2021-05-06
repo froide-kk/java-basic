015bc63c200a310bd0b496f4aae5a86f070a350d
“yamato-dejimau” <yamato-dejima@froide.co.jp>
2021-05-06 17:30:20 +0900

    [WIP]update:練習課題2
    練習課題2-5を作成途中

--- src/exercises/problem2/Problem2_5.java
+++ src/exercises/problem2/Problem2_5.java
@@ -5,9 +5,22 @@ public class Problem2_5 {
         String temp = "こんばんわおはよう";
         String ex_temp = "こおはんばんようわ";
         // tempを「おはよう」と「こんばんわ」に分割
+        String morning_temp = temp.replace("こんばんわ","");
+        String afternoon_temp = temp.replace("おはよう","");
+        String morning_ex_temp = "";
+        String afternoon_ex_temp = "";
 
         // ex_tempを「おはよう」と「こんばんわ」に分割
+        String[] list = ex_temp.split("");
+        list.toString();
 
-        System.out.println();
+        for (int i = 0; i <ex_temp.length() ; i++) {
+            System.out.println(list[i] == "お");
+        }
+        System.out.println(morning_temp);
+        System.out.println(afternoon_temp);
+        System.out.println(morning_ex_temp);
+        System.out.println(afternoon_ex_temp);
+
     }
 }
