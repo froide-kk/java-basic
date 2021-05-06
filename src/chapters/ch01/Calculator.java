58eefcc0b4ce91b225101680b8f4703c44e20eaf
chiharu-ono <chiharu-ono@froide.co.jp>
2021-05-06 11:26:04 +0900

    ch01の変更

--- src/chapters/ch01/Calculator.java
+++ src/chapters/ch01/Calculator.java
@@ -5,8 +5,9 @@ public class Calculator {
 		/* 計算をする */
 		int a = 1;
 		int b = 2;
-		int c = a + b; // cは3になる
+		int c = a - b; // cは-1になる
+		int d = c + a;
 		/* 計算した結果を表示する */
-		System.out.println("1 + 2 = " + c);
+		System.out.println("1 - 2 + 1 = " + d);
 	}
 }
