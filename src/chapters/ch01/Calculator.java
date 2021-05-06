8d257d66cc03776d9bfae70a4d8b2a8dc948199d
froide-nagayoshi <yudai-nagayoshi@froide.co.jp>
2021-05-06 11:28:58 +0900

    add:変更完了

--- src/chapters/ch01/Calculator.java
+++ src/chapters/ch01/Calculator.java
@@ -3,10 +3,12 @@ package chapters.ch01;
 public class Calculator {
 	public static void main(String[] args) {
 		/* 計算をする */
-		int a = 1;
-		int b = 2;
-		int c = a + b; // cは3になる
+		int i = 0;
+		for(;i<10;i++){
+			i++;
+		}
+		 // cは3になる
 		/* 計算した結果を表示する */
-		System.out.println("1 + 2 = " + c);
+		System.out.println(i);
 	}
 }
