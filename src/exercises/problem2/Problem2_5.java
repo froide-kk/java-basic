b6711fdd2c7037be22f69f2b39ced89ef9bee8f2
chiharu-ono <chiharu-ono@froide.co.jp>
2021-05-06 17:30:22 +0900

    [update]problem2-2, 2-5の変更

--- src/exercises/problem2/Problem2_5.java
+++ src/exercises/problem2/Problem2_5.java
@@ -5,18 +5,32 @@ public class Problem2_5 {
         String temp = "こんばんわおはよう";
         String ex_temp = "こおはんばんようわ";
         // tempを「おはよう」と「こんばんわ」に分割
-        String temp1 = temp.substring(5, 9);
-        String temp2 = temp.substring(0, 5);
+        String ohayo1 = temp.substring(5, 9);
+        String konbanwa1 = temp.substring(0, 5);
 
         // ex_tempを「おはよう」と「こんばんわ」に分割
-        String ex_temp1 = ex_temp.substring(1, 3) + ex_temp.substring(6, 8);
-        String ex_temp2 = ex_temp.charAt(0) + ex_temp.substring(3, 6) + ex_temp.charAt(8);
+        String ohayo2 = ex_temp.substring(1, 3) + ex_temp.substring(6, 8);
+        String konbanwa2 = ex_temp.charAt(0) + ex_temp.substring(3, 6) + ex_temp.charAt(8);
 
-        String ex_temp3 = ex_temp.replace("こ", "").replace("んばん", "").replace("わ", "");
-        String ex_temp4 = ex_temp.replace("おは", "").replace("よう", "");
+        String ohayo3 = ex_temp.replace("こ", "").replace("んばん", "").replace("わ", "");
+        String konbanwa3 = ex_temp.replace("おは", "").replace("よう", "");
 
-        System.out.println(temp1 + "\n" + temp2);
-        System.out.println(ex_temp1 + "\n" + ex_temp2);
-        System.out.println(ex_temp3 + "\n" + ex_temp4);
+        String ohayo4 =  "";
+        String konbanwa4 = "";
+        String dummy;
+        for(int i=0; i<ex_temp.length(); i++){
+            dummy = "";
+            dummy += ex_temp.charAt(i);
+            if (dummy.equals("お") || dummy.equals("は") || dummy.equals("よ") || dummy.equals("う")){
+                ohayo4 += dummy;
+            } else {
+                konbanwa4 += dummy;
+            }
+        }
+
+        System.out.println(ohayo1 + "\n" + konbanwa1);
+        System.out.println(ohayo2 + "\n" + konbanwa2);
+        System.out.println(ohayo3 + "\n" + konbanwa3);
+        System.out.println(ohayo4 + "\n" + konbanwa4);
     }
 }
