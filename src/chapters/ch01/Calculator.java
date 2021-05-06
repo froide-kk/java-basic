081bf7498164e1b394c9f0696bff98029dc984f7
“yamato-dejimau” <yamato-dejima@froide.co.jp>
2021-05-06 11:28:43 +0900

    update:オペランドを変更

--- src/chapters/ch01/Calculator.java
+++ src/chapters/ch01/Calculator.java
@@ -3,10 +3,10 @@ package chapters.ch01;
 public class Calculator {
 	public static void main(String[] args) {
 		/* 計算をする */
-		int a = 1;
+		int a = 3;
 		int b = 2;
 		int c = a + b; // cは3になる
 		/* 計算した結果を表示する */
-		System.out.println("1 + 2 = " + c);
+		System.out.println("3 + 2 = " + c);
 	}
 }
