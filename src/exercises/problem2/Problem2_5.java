ac8a22d9df041944d7ec9b71634234a9255ac07b
froide-minori <minori-hagihara@froide.co.jp>
2021-05-06 17:39:28 +0900

    2-5 変更

--- src/exercises/problem2/Problem2_5.java
+++ src/exercises/problem2/Problem2_5.java
@@ -8,15 +8,8 @@ public class Problem2_5 {
         String evening = temp.substring(0,5);
         String morning = temp.substring(5);
         // ex_tempを「おはよう」と「こんばんわ」に分割
-        String ohayo1 = ex_temp.substring(1,3);
-        String ohayo2 = ex_temp.substring(6,8);
-        String ohayo = ohayo1 + ohayo2;
-
-        String konbanwa1 = ex_temp.substring(0,1);
-        String konbanwa2 = ex_temp.substring(3,6);
-        String konbanwa3 = ex_temp.substring(8);
-        String konbanwa = konbanwa1 + konbanwa2 + konbanwa3;
-
+        String konbanwa = ex_temp.replace("こおはんばんよう","こんばん");
+        String ohayo = ex_temp.replaceAll("[あ-ん]+","おはよう");
 
         System.out.println(morning);
         System.out.println(evening);
