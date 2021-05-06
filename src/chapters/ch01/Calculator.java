9b15789f0e66a6406c43d5a358b2bc0098a740a8
froide-asao <kazutoshi-asao@froide.co.jp>
2021-05-06 11:25:53 +0900

    Chap1を変更

--- src/chapters/ch01/Calculator.java
+++ src/chapters/ch01/Calculator.java
@@ -5,8 +5,12 @@ public class Calculator {
 		/* 計算をする */
 		int a = 1;
 		int b = 2;
-		int c = a + b; // cは3になる
+		for(int i = a; i<10; i++){
+			int c = i + b;
+			System.out.printf("%d + 2 = %d\n", i,c);
+		}
+		 // cは3になる
 		/* 計算した結果を表示する */
-		System.out.println("1 + 2 = " + c);
+
 	}
 }
