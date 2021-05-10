cdfea34a9e4005e6aea0685c477aa394b9572dda
froide-nagayoshi <yudai-nagayoshi@froide.co.jp>
2021-05-10 12:07:49 +0900

    add:prblem5

--- src/exercises/problem5/Problem5_1.java
+++ src/exercises/problem5/Problem5_1.java
@@ -1,6 +1,17 @@
 package exercises.problem5;
 
+import java.util.Random;
+
 public class Problem5_1 {
 	public static void main(String[] args){
+		int[] arr = new int[7];
+		Random rn = new Random();
+		for(int i=0;i<arr.length;i++){
+			arr[i] = rn.nextInt(10)+1;
+		}
+		for(int i=0;i<arr.length;i++){
+			System.out.print("arr["+i+"]="+arr[i]+" ");
+		}
+
 	}
 }
--- src/exercises/problem5/Problem5_5.java
+++ src/exercises/problem5/Problem5_5.java
@@ -1,6 +1,39 @@
 package exercises.problem5;
 
+import java.util.Random;
+
 public class Problem5_5 {
 	public static void main(String[] args){
+		int[] arr = new int[5];
+		Random rn = new Random();
+
+
+		for(int i=0;i<arr.length;i++){
+			arr[i] = rn.nextInt(20)-10;
+		}
+		for(int i: arr){
+			System.out.print(i+" ");
+		}
+		System.out.println();
+
+		int maxcount = 0;
+		int mincount = 0;
+		int zerocount = 0;
+		for(int i=0;i<arr.length;i++){
+			if(arr[i]>0){
+				maxcount++;
+			}
+			if(arr[i]<0){
+				mincount++;
+			}
+			if(arr[i]==0){
+				zerocount++;
+			}
+		}
+		System.out.println("0より大きい数："+maxcount+"個");
+		System.out.println("0より小さい数："+mincount+"個");
+		System.out.println("0の個数："+zerocount+"個");
+
+
 	}
 }
