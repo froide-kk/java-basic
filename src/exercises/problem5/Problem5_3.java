715cc02b9aeeaab6b77d2c0f9e27ec4ae1964897
chiharu-ono <chiharu-ono@froide.co.jp>
2021-05-10 11:40:31 +0900

    [update]problem5の変
    更

--- src/exercises/problem5/Problem5_3.java
+++ src/exercises/problem5/Problem5_3.java
@@ -1,6 +1,25 @@
 package exercises.problem5;
+import java.util.Random;
 
 public class Problem5_3 {
 	public static void main(String[] args){
+		Random rand = new Random();
+		String kisu = "";
+		String gusu = "";
+		int[] data = new int[10];
+		for(int i = 0; i < data.length; i++){
+			data[i] = rand.nextInt(100) + 1;
+			System.out.print(data[i] + " ");
+		}
+		System.out.println();
+		for(int j = 0; j < data.length; j++){
+			if (data[j] % 2 == 0){
+				gusu += (data[j] + " ");
+			} else {
+				kisu += (data[j] + " ");
+			}
+		}
+		System.out.println("奇数：" + kisu);
+		System.out.println("偶数：" + gusu);
 	}
 }
--- src/exercises/problem5/Problem5_5.java
+++ src/exercises/problem5/Problem5_5.java
@@ -1,6 +1,32 @@
 package exercises.problem5;
+import java.util.Random;
 
 public class Problem5_5 {
 	public static void main(String[] args){
+		Random rand = new Random();
+		int[] data = new int[5];
+		//challenge問題
+		//int num = rand.nextInt(10)+1;
+		//int[] data = new int[num];
+		for (int i = 0; i < data.length; i++){
+			data[i] = rand.nextInt(21) - 10;
+			System.out.print(data[i] + " ");
+		}
+
+		int count_big = 0;
+		int count_small = 0;
+		int count_zero = 0;
+		for (int j = 0; j < data.length; j++){
+			if (0 < data[j]){
+				count_big += 1;
+			} else if (data[j] < 0){
+				count_small += 1;
+			} else {
+				count_zero += 1;
+			}
+		}
+		System.out.println("\n\n0より大きい数：" + count_big + "個");
+		System.out.println("0より小さい数：" + count_small + "個");
+		System.out.println("0の個数：" + count_zero + "個");
 	}
 }
