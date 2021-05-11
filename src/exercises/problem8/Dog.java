c2519b85f244c54b8062d845e1d01f64d960aacc
chiharu-ono <chiharu-ono@froide.co.jp>
2021-05-11 11:22:46 +0900

    [update]problem8の変更

--- src/exercises/problem8/Dog.java
+++ src/exercises/problem8/Dog.java
@@ -1,13 +1,30 @@
 package exercises.problem8;
 
 public class Dog {
+    Dog( String breed ){
+        this.breed = breed;
+    }
+
+    Dog(){
+
+    }
     private String name;
+
+    private int age;
+
+    private String breed = "不明";
 
     public void setName( String name ) {
         this.name = name;
     }
 
+    public void setAge( int age ){
+        this.age = age;
+    }
+
     public void showProfile() {
         System.out.println( "名前は、" + this.name + "です。" );
+        System.out.println( "年齢は、" + this.age + "歳です。" );
+        System.out.println( "犬種は、" + this.breed + "です。" );
     }
 }
--- src/exercises/problem8/Problem8_3.java
+++ src/exercises/problem8/Problem8_3.java
@@ -1,4 +1,10 @@
 package exercises.problem8;
 
 public class Problem8_3 {
+    public static void main(String[] args){
+        Dog dog = new Dog("柴犬");
+        dog.setName("ハチ");
+        dog.setAge(6);
+        dog.showProfile();
+    }
 }
