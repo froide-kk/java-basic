4056d0e6651265b6a10986458cd0138fca1c98e3
chiharu-ono <chiharu-ono@froide.co.jp>
2021-05-11 11:49:50 +0900

    [update]problem9の変更

--- src/exercises/problem9/Problem9_1.java
+++ src/exercises/problem9/Problem9_1.java
@@ -1,4 +1,11 @@
 package exercises.problem9;
 
 public class Problem9_1 {
+    public static void main(String[] args){
+        Cat cat = new Cat();
+        cat.setName("さくら");
+        cat.setAge(12);
+        cat.showProfile();
+        cat.sleep();
+    }
 }
--- src/exercises/problem9/Problem9_3.java
+++ src/exercises/problem9/Problem9_3.java
@@ -1,4 +1,20 @@
 package exercises.problem9;
 
 public class Problem9_3 {
+    public static void main(String[] args){
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+
+        cat.setName("さくら");
+        cat.setAge(12);
+
+        dog.setName("ハチ");
+        dog.setAge(6);
+
+        cat.showProfile();
+        dog.showProfile();
+
+        cat.speak();
+        dog.speak();
+    }
 }
