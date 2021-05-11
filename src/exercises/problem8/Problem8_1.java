aa6fe02dc7ef1eddee2eeda267056cfeac20f2b5
froide-nagayoshi <yudai-nagayoshi@froide.co.jp>
2021-05-11 11:56:23 +0900

    add:problem7,8

--- src/exercises/problem8/Problem8_1.java
+++ src/exercises/problem8/Problem8_1.java
@@ -1,4 +1,20 @@
 package exercises.problem8;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+
 public class Problem8_1 {
+    public static void main(String[] args) throws IOException{
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+
+        System.out.print("名前を入力:");
+        String str = br.readLine();
+        Dog doc = new Dog();
+
+        doc.setName(str);
+        doc.showProfile();
+    }
 }
--- src/exercises/problem8/Problem8_2.java
+++ src/exercises/problem8/Problem8_2.java
@@ -1,4 +1,22 @@
 package exercises.problem8;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Problem8_2 {
+    public static void main(String[] args) throws IOException{
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        Dog dog = new Dog();
+
+        System.out.print("名前を入力:");
+        String name = br.readLine();
+        System.out.print("年齢を入力:");
+        int age = Integer.parseInt(br.readLine());
+
+        dog.setName(name);
+        dog.setAge(age);
+        dog.showProfile();
+
+    }
 }
