cd6d1de871df3c80ab49d4a17486a4320b9e86d4
池田りお <rio-ikeda@froide.co.jp>
2021-05-12 16:00:19 +0900

    10-2追加

--- src/exercises/problem10/Problem10_2.java
+++ src/exercises/problem10/Problem10_2.java
@@ -13,6 +13,7 @@ public class Problem10_2 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         ArrayList<Integer> arr = new ArrayList<Integer>();
 
+//        100回まで打ち間違いok
         for(int j = 0; j < 100; j++) {
             int i = 1;
             try {
@@ -30,35 +31,23 @@ public class Problem10_2 {
             }
         }
         System.out.println(arr);
+        Collections.sort(arr, Collections.reverseOrder());
+        System.out.println(arr);
+        int big = arr.get(0);
+        int small = arr.get(1);
+        int div = big % small;
+        System.out.println(div);
 
+        while(div != 0) {
+            big = small;
+            small = div;
+            return;
+        }
+
+//        System.out.println(small);
+//        ここまで
+        
+        System.out.println("あああああ");
     }
-
-
-//        while (i < 3) {
-//            System.out.println("自然数(" + i + "つ目)を指定：");
-//            String str = br.readLine();
-//            int num = Integer.parseInt(str);
-//            try {
-//                System.out.println(num);
-//                i++;
-//                arr.add(num);
-//            } catch (NumberFormatException e) {
-//                System.out.println("自然数を指定して下さい");
-//            }
-//        }
-
-//        System.out.println(arr);
-//        二つ入力した後で数値の判定
-
-
-
-
-//        余りで割る数を割る
-//        もし1つ目(a)>2つ目(b)の場合
-//        a % b = c;
-//        b % c = d;
-//        c % d = e;
-//        e = 0になった時のd
-
-
 }
+
