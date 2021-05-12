13be4ca6c5d3920e56be608e2bbd15e36964b84e
froide-nagayoshi <yudai-nagayoshi@froide.co.jp>
2021-05-12 16:02:44 +0900

    add:problem10-1~10-3

--- src/exercises/problem10/Problem10_2.java
+++ src/exercises/problem10/Problem10_2.java
@@ -1,13 +1,31 @@
 package exercises.problem10;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
  * 最大公約数
  */
 public class Problem10_2 {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("自然数(一つ目)を指定：");
+        int natural1 = Integer.parseInt(br.readLine());
+        System.out.print("自然数(２つ目)を指定：");
+        int natural2 = Integer.parseInt(br.readLine());
+
+        int i=2;
+        while(true){
+            if(natural1%i==0&&natural2%i==0){
+                System.out.println("最大公約数は"+i);
+                break;
+            }
+            i++;
+        }
+
+
+
     }
 }
