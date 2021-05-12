c02656e72bb0521f573efda588faaa9d252335cf
“yamato-dejimau” <yamato-dejima@froide.co.jp>
2021-05-12 17:30:44 +0900

    add:練習問題10-3
    練習問題10-3まで作成しました。

--- src/exercises/problem10/Problem10_2.java
+++ src/exercises/problem10/Problem10_2.java
@@ -2,12 +2,35 @@ package exercises.problem10;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * 最大公約数
  */
 public class Problem10_2 {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> list = new ArrayList<>();
+
+        //  コンソールから文字列の読み込み
+        while (true){
+            int operand1 = 0;
+            System.out.print("文字を入力：");
+            String str = br.readLine();
+            list.add(Integer.parseInt(str));
+            if (list.size()==2) {
+                while (true){
+                    operand1=list.get(0) % list.get(1);
+                    if (operand1 == 0){
+                        System.out.print("最大公約数: "+list.get(1)+"\n");
+                        break;
+                    }
+                    list.set(0,list.get(1));
+                    list.set(1,operand1);
+                }
+            }
+
+        }
     }
 }
--- src/exercises/problem10/Problem10_3.java
+++ src/exercises/problem10/Problem10_3.java
@@ -3,6 +3,8 @@ package exercises.problem10;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Convert repeated characters to numbers
@@ -10,11 +12,31 @@ import java.io.InputStreamReader;
 public class Problem10_3 {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
+        int count = 1;
+        String text = "";
+        String result = "";
 
-    }
+            System.out.print("文字を入力：");
+            String str = br.readLine();
+            List<String> list = Arrays.asList(str.split(""));
 
-    static String convertRepeatedCharactersToNumbers(String s) {
-        return "";
+            for (int i = 0; i < list.size(); i++) {
+                if (result.equals(list.get(i))) {
+                    count++;
+                    if (i+1 == list.size()) {
+                        text += count;
+                    }
+                    continue;
+                }
+                if (count > 1) {
+                    text += count;
+                    count = 1;
+                }
+                text += list.get(i);
+                result = list.get(i);
+            }
+            System.out.print("Input："+str+"\n");
+            System.out.print("Output："+text);
+
     }
 }
