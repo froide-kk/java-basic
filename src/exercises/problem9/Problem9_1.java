31d2a813e19716cf44af0c3131db1376366fe3ed
“yamato-dejimau” <yamato-dejima@froide.co.jp>
2021-05-12 14:17:21 +0900

    add:練習問題9
    練習問題9を作成しました。

--- src/exercises/problem9/Problem9_1.java
+++ src/exercises/problem9/Problem9_1.java
@@ -1,4 +1,11 @@
 package exercises.problem9;
 
 public class Problem9_1 {
+    public static void main(String[] args){
+        Cat cat = new Cat();
+        cat.name = "hoge";
+        cat.age = 19;
+        cat.showProfile();
+        cat.sleep();
+    }
 }
--- src/exercises/problem9/Problem9_2.java
+++ src/exercises/problem9/Problem9_2.java
@@ -1,4 +1,16 @@
 package exercises.problem9;
 
 public class Problem9_2 {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.name = "hoge";
+        cat.age = 19;
+        cat.showProfile();
+        cat.sleep();
+        Dog dog = new Dog();
+        dog.name = "hoge丸";
+        dog.age = 89;
+        dog.showProfile();
+        dog.run();
+    }
 }
