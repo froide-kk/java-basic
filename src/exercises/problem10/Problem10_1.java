f9d93709e03d3dba6aa75e59d4052e55295caa5c
chiharu-ono <chiharu-ono@froide.co.jp>
2021-05-12 13:39:57 +0900

    [update]problem9,10-1,10-2,10-3の変更

--- src/exercises/problem10/Problem10_1.java
+++ src/exercises/problem10/Problem10_1.java
@@ -4,4 +4,17 @@ package exercises.problem10;
  * FizzBuzz
  */
 public class Problem10_1 {
+    public static void main(String[] args){
+        for (int i = 1; i <= 30; i++){
+            if (i % (3*5) == 0){
+                System.out.print("FizzBuzz ");
+            } else if (i % 3 == 0){
+                System.out.print("Fizz ");
+            } else if (i % 5 == 0){
+                System.out.print("Buzz ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
