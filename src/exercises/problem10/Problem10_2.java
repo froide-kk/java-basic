5b60613a5020f736044ab856e725f2414c94ae5d
froide-minori <minori-hagihara@froide.co.jp>
2021-05-12 17:35:04 +0900

    演習10-1の修正

--- src/exercises/problem10/Problem10_2.java
+++ src/exercises/problem10/Problem10_2.java
@@ -2,6 +2,7 @@ package exercises.problem10;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /**
  * 最大公約数
@@ -9,5 +10,18 @@ import java.io.InputStreamReader;
 public class Problem10_2 {
     public static void main(String args[]) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] num = new int[2];
+
+        String str = br.readLine();
+
+        for (int i = 0; i < 2; i++) {
+            if()
+
+        }
+
+
+
+            }
+        }
     }
 }
